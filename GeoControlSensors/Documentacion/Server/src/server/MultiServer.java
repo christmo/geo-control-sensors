b3d092bf822ddd9efb43b02c7ad0eea5ca066ab3
christmo99 <christmo99@73f47bf4-9d0a-6a80-f8a3-2307588149c8>
2011-05-30 22:57:45 +0000

    Incio del proyecto para monitoreo de sensores

---
+++ GeoControlSensors/Documentacion/Server/src/server/MultiServer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MultiServer {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+
+        try {
+            serverSocket = new ServerSocket(20);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(-1);
+        }
+
+        while (listening) {
+            new MultiServerThread(serverSocket.accept()).start();
+        }
+
+        serverSocket.close();
+    }
+}
