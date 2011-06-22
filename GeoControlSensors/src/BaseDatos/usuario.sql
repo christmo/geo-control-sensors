47f0dff5fedad70f2eb92398f1be25f78b82099e
christmo99 <christmo99@73f47bf4-9d0a-6a80-f8a3-2307588149c8>
2011-06-22 19:37:28 +0000

    usuario en la base para eliminar contactos

--- GeoControlSensors/src/BaseDatos/usuario.sql
+++ GeoControlSensors/src/BaseDatos/usuario.sql
@@ -2,3 +2,9 @@ GRANT INSERT, SELECT, EXECUTE
 ON monitorsensores.*
 TO 'sensor'@'127.0.0.1'
 IDENTIFIED BY 'sensor';
+
+
+GRANT DELETE
+ON monitorsensores.contactos
+TO 'sensor'@'127.0.0.1'
+IDENTIFIED BY 'sensor';
