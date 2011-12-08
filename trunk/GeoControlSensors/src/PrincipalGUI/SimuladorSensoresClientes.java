e4dfe376438373e0c7e133b3ca1430f17d77fef3
christmo99 <christmo99@73f47bf4-9d0a-6a80-f8a3-2307588149c8>
2011-12-08 17:48:40 +0000

    Nuevo Icono Temperatura

--- trunk/GeoControlSensors/src/PrincipalGUI/SimuladorSensoresClientes.java
+++ trunk/GeoControlSensors/src/PrincipalGUI/SimuladorSensoresClientes.java
@@ -38,7 +38,7 @@ public class SimuladorSensoresClientes {
             while (true) {
                 //demo = "M01%T1&" + (int)(Math.random() * 30) + "%T2&" + (int)(Math.random() * 30) + "%T3&" + (int)(Math.random() * 30) + "%T4&" + (int)(Math.random() * 30)+"\n";
                 //demo = "M01%T1&" + (int) (Math.random() * 30) + "%T2&" + (int) (Math.random() * 30) + "\n";
-                demo = "M01%T1&" + (Math.random() * 100)+ "\n";
+                demo = "M01%T2&" + (Math.random() * 30)+ "\n";
                 System.out.print("" + demo);
                 ou.write(demo.getBytes());
                 try {
