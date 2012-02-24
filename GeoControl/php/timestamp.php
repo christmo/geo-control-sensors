9d3950fe8a67e57d73ae3ff91e7cd387e2750c95
christmo99 <christmo99@73f47bf4-9d0a-6a80-f8a3-2307588149c8>
2012-02-24 15:20:59 +0000

    /**  * Se incrementa 5 horas a la hora de visualización para igualar con la hora  * timestamp del servidor cuando php en la directiva de DATE no tiene la hora UTC,  * esto se hace porque FLOT el componente grafico trabaja todo en hora UTC las  * series de tiempo se generan automáticamente con la consulta timestamp de la base  *  * [Date]  * date.timezone = UTC  *  * Y esta configurado de manera local  *  * [Date]  * date.timezone = "America/Guayaquil"  */ /**  * Poner a 0 esta variable si el archivo php.ini tiene el date.timezone = UTC  */

--- GeoControl/php/timestamp.php
+++ GeoControl/php/timestamp.php
@@ -1,13 +1,31 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/*
+ * Este archivo hace la conversion de la hora en timestamp a horas, minutos y segundos
+ * para que sea presentada en el popup de la grafica
  */
 
 extract($_GET);
 
-//echo $hora."<br/>";
-$ar = ($hora/1000);
-echo date('H:i:s',$ar);
+/**
+ * Se incrementa 5 horas a la hora de visualización para igualar con la hora 
+ * timestamp del servidor cuando php en la directiva de DATE no tiene la hora UTC,
+ * esto se hace porque FLOT el componente grafico trabaja todo en hora UTC las
+ * series de tiempo se generan automáticamente con la consulta timestamp de la base
+ * 
+ * [Date]
+ * date.timezone = UTC
+ * 
+ * Y esta configurado de manera local
+ * 
+ * [Date]
+ * date.timezone = "America/Guayaquil"
+ */
+/**
+ * Poner a 0 esta variable si el archivo php.ini tiene el date.timezone = UTC
+ */
+$incrementoUTC = (5 * 60 * 60 * 1000);
 
+$ar = (($hora + $incrementoUTC) / 1000);
+echo date('H:i:s', $ar);
 ?>
